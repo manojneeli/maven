83a21f86bb8b17dff028b4502a30537b8976b74a
manojneeli <91675113+manojneeli@users.noreply.github.com>
2022-02-01 16:51:59 +0530

    first base code

---
+++ src/main/java/com/democompany2/second_demo_project/App.java
@@ -0,0 +1,13 @@
+package com.democompany2.second_demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
